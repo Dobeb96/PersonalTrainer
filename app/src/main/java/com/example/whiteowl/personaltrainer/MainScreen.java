8d3d6566970dab3c4caeaaf5b5d78d8c55eba912
Doberman96 <dobosz.ma@interia.pl>
2017-04-08 16:33:26 +0200

    Update MainScreen.java
    Update v6

--- app/src/main/java/com/example/whiteowl/personaltrainer/MainScreen.java
+++ app/src/main/java/com/example/whiteowl/personaltrainer/MainScreen.java
@@ -18,6 +18,7 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         // tutaj sobie coś pozmieniam
         // Kamil to fajny chłopak
+        // Sprawdzam
     }
 
     public void add(View view) {
