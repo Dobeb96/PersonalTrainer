916ec9c4a9e9e09f609b3a24903b983106d18223
Doberman96 <dobosz.ma@interia.pl>
2017-04-08 16:49:11 +0200

    Update DisplayMessageActivity.java
    pkoko

--- app/src/main/java/com/example/whiteowl/personaltrainer/DisplayMessageActivity.java
+++ app/src/main/java/com/example/whiteowl/personaltrainer/DisplayMessageActivity.java
@@ -11,7 +11,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
+       // edit
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainScreen.EXTRA_MESSAGE);
 
