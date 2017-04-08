f7946df607c0df3eaed234c77a5cac68e2d3e948
dobeb <maciej.dobosz_md@outlook.com>
2017-04-08 16:44:02 +0200

    Update v7

--- app/src/main/java/com/example/whiteowl/personaltrainer/MainScreen.java
+++ app/src/main/java/com/example/whiteowl/personaltrainer/MainScreen.java
@@ -18,12 +18,6 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         // tutaj sobie coś pozmieniam
         // Kamil to fajny chłopak
-
-
-
-
-
-        /////
         // Sprawdzam
     }
 
